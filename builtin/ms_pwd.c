0814c432be6c79e6b2111db5093dea5068ee62cc
Ryomanba <datemanba3@gmail.com>
2021-08-24 14:40:23 +0900

    pwd

---
+++ builtin/ms_pwd.c
@@ -0,0 +1,17 @@
+#include "ms_builtin.h"
+
+int	ms_pwd(void)
+{
+	char	*path;
+
+	errno = 0; // 初期化する?
+	path = getcwd(NULL, 0); // 第一引数がNULLだと動的に確保される(free必要)
+	if (errno != 0)
+	{
+		perror("pwd");
+		return (1);
+	}
+	printf("%s\n", path);
+	free(path);
+	return (0);
+}
