44ff7ef5fb2a5e76b0d0d1997c854c36d9260018
河田鷹介 <benefium@gmail.com>
2021-09-22 23:10:36 +0900

    閉じてないクオートはシンタックスエラーとする https://github.com/ryo-manba/minishell/issues/41

--- analyzer/lx_cursor.c
+++ analyzer/lx_cursor.c
@@ -6,11 +6,38 @@
 /*   By: yokawada <yokawada@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 02:12:20 by yokawada          #+#    #+#             */
-/*   Updated: 2021/09/12 19:07:22 by yokawada         ###   ########.fr       */
+/*   Updated: 2021/09/22 22:49:30 by yokawada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ms_analyzer.h"
+
+void	lx_error_prologue(t_lex_cursor *cursor)
+{
+	ft_putstr_fd(cursor->master->prog_name, STDERR_FILENO);
+	if (!cursor->master->interactive_shell)
+	{
+		if (cursor->master->opt_c)
+			ft_putstr_fd(": -c", STDERR_FILENO);
+		ft_putstr_fd(": line ", STDERR_FILENO);
+		ft_putsizet_fd(cursor->master->line_num, STDERR_FILENO);
+	}
+	ft_putstr_fd(": ", STDERR_FILENO);
+}
+
+t_wdlist	*lx_unclosed_quote(t_lex_cursor *cursor)
+{
+	lx_error_prologue(cursor);
+	ft_putstr_fd("unexpected EOF while looking for matching `", STDERR_FILENO);
+	ft_putchar_fd(cursor->under_quoted, STDERR_FILENO);
+	ft_putstr_fd("'\n", STDERR_FILENO);
+	cursor->master->line_num += 1;
+	lx_error_prologue(cursor);
+	ft_putstr_fd("syntax error: unexpected end of file\n", STDERR_FILENO);
+	g_ex_states = 2;
+	lx_destroy_token(cursor->head);
+	return (NULL);
+}
 
 int	lx_mark_failed(t_lex_cursor *cursor, int mark, char *message)
 {
@@ -39,8 +66,10 @@ t_wdlist	*lx_finalize(t_lex_cursor *cursor)
 	if (cursor->failed)
 	{
 		lx_destroy_token(cursor->head);
-		cursor->head = NULL;
+		return (NULL);
 	}
+	else if (cursor->under_quoted)
+		return (lx_unclosed_quote(cursor));
 	else
 		lx_conclude_token(cursor);
 	if (cursor->tail && cursor->tail->lex_type != LT_NEWLINE)
