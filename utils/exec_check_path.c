50546f4c871bcceb67a94f5b1333e7428ab2d49c
河田鷹介 <benefium@gmail.com>
2021-09-23 18:13:20 +0900

    bugfix: Read権限のないファイルを実行しようとした時にpermission deniedとする https://github.com/ryo-manba/minishell/issues/41

--- utils/exec_check_path.c
+++ utils/exec_check_path.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_check_path.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmatsuka <rmatsuka@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: yokawada <yokawada@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 15:32:33 by rmatsuka          #+#    #+#             */
-/*   Updated: 2021/09/15 15:53:12 by rmatsuka         ###   ########.fr       */
+/*   Updated: 2021/09/23 18:00:23 by yokawada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	exec_check_path(char *path, int is_relative)
 	}
 	if (S_ISREG(sb.st_mode))
 	{
-		if ((sb.st_mode & S_IXUSR))
+		if ((sb.st_mode & S_IXUSR) && (sb.st_mode & S_IRUSR))
 			return (MS_EXEC_SUCC);
 		g_ex_states = PERMISSION;
 	}
@@ -44,8 +44,10 @@ int	exec_check_path(char *path, int is_relative)
 int	exec_check_relative(char *path)
 {
 	struct stat	sb;
+	int			rv;
 
-	if (stat(path, &sb) < 0 || S_ISDIR(sb.st_mode))
+	rv = stat(path, &sb);
+	if (rv < 0 || S_ISDIR(sb.st_mode))
 	{
 		g_ex_states = CMD_NOT_FOUND;
 		return (MS_EXEC_FAIL);
