24a75d80aae0f62bce9446ed34089b74c8edd5a3
Ryomanba <datemanba3@gmail.com>
2021-09-30 21:56:58 +0900

    コマンド実行後のclose処理

--- utils/exec_backup_fd.c
+++ utils/exec_backup_fd.c
@@ -6,7 +6,7 @@
 /*   By: rmatsuka <rmatsuka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 00:08:03 by rmatsuka          #+#    #+#             */
-/*   Updated: 2021/09/30 21:39:36 by rmatsuka         ###   ########.fr       */
+/*   Updated: 2021/09/30 21:52:06 by rmatsuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,10 @@ int	exec_create_backup_fd(int backup_fd[3])
 	backup_fd[1] = dup(STDOUT_FILENO);
 	backup_fd[2] = dup(STDERR_FILENO);
 	if (backup_fd[0] == -1 || backup_fd[1] == -1 || backup_fd[2] == -1)
+	{
+		ms_perror("dup");
 		return (1);
+	}
 	return (0);
 }
 
@@ -42,13 +45,13 @@ int	exec_duplicate_backup_fd(int backup_fd[3])
 	int	ret_zero;
 	int	ret_one;
 	int	ret_two;
-	int	ret_three;
+	int	ret_close;
 
 	ret_zero = dup2(backup_fd[0], STDIN_FILENO);
 	ret_one = dup2(backup_fd[1], STDOUT_FILENO);
 	ret_two = dup2(backup_fd[2], STDERR_FILENO);
-	ret_three = exec_close_backup_fd(backup_fd);
-	if (ret_zero || ret_one || ret_two || ret_three)
+	ret_close = exec_close_backup_fd(backup_fd);
+	if (ret_zero == -1 || ret_one == -1 || ret_two == -1 || ret_close)
 		return (1);
 	return (0);
 }
